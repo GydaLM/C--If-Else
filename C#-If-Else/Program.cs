df04d7392afd7e0913b26bee053abf33275355dd
Lonatri <gyda-94@hotmail.com>
2025-11-19 12:41:01 +0100

    Add project files.

---
+++ C#-If-Else/Program.cs
@@ -0,0 +1,28 @@
+﻿
+CheckIfEqual();
+static void CheckIfEqual()
+{
+    int number1 = 3;
+    int number2 = 2;
+    bool isEqual = false;
+    if(number1 == number2)
+    {
+        isEqual = true;
+        Console.WriteLine("The numbers are equal");
+    }
+    else
+    {
+        isEqual = false;
+        Console.WriteLine("The numbers are not equal");
+    }
+    if (isEqual)
+    {
+        number1++;
+    }
+    else
+    {
+        number2--;
+    }
+    Console.WriteLine($"This is number1: {number1} \nand this is number2: {number2}");
+    Console.WriteLine($"This is the boolean: {isEqual}");
+}
